0151a3c0742d13b2e71c6e7ad9b99f9a22c7cacd
hrkn63-hnm <demo43160@gmail.com>
2022-07-28 00:41:18 +0900

    fire.jsのexport

--- components/fire.js
+++ components/fire.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
