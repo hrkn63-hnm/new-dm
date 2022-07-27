cf8c2bebbcfb68c5750deff8fb9b21b20a6dec54
hrkn63-hnm <demo43160@gmail.com>
2022-07-27 14:55:04 +0900

    first commit

---
+++ components/fire.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0_8OZjPM3oKfjq0dor8LdaSLFjMcfruw",
+  authDomain: "new-dm-52824.firebaseapp.com",
+  projectId: "new-dm-52824",
+  storageBucket: "new-dm-52824.appspot.com",
+  messagingSenderId: "7593387817",
+  appId: "1:7593387817:web:99fa5e618588cdb5e67938"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ pages/address/add.js
@@ -0,0 +1,105 @@
+import { useState, useEffect } from "react";
+import Layout from "../../components/layout";
+import { getFirestore, setDoc, doc } from "firebase/firestore/lite";
+import { getAuth } from "firebase/auth";
+import { useRouter } from "next/router";
+import "../../components/fire";
+
+const db = getFirestore();
+const auth = getAuth();
+
+export default function Add() {
+  const [message] = useState("add address");
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [tel, setTel] = useState("");
+  const [memo, setMemo] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (auth.currentUser == null) {
+      router.push("/address");
+    }
+  }, []);
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeMail = (e) => {
+    setMail(e.target.value);
+  };
+  const onChangeTel = (e) => {
+    setTel(e.target.value);
+  };
+  const onChangeMemo = (e) => {
+    setMemo(e.target.value);
+  };
+
+  const doAction = (_) => {
+    const ob = {
+      name: name,
+      mail: mail,
+      tel: tel,
+      memo: memo,
+      flag: false,
+    };
+    setDoc(
+      doc(db, "address", auth.currentUser.email, "address", mail),
+      ob
+    ).then((_) => {
+      router.push("/address");
+    });
+  };
+
+  const goBack = (_) => {
+    router.push("/address");
+  };
+
+  return (
+    <>
+      <Layout header="Next.js" title="Create data.">
+        <div className="alert alert-primary text-center">
+          <h5 className="mb-4">{message}</h5>
+          <div className="form-group">
+            <label>Name:</label>
+            <input
+              type="text"
+              onChange={onChangeName}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label>Mail:</label>
+            <input
+              type="text"
+              onChange={onChangeMail}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label>Tel:</label>
+            <input
+              type="text"
+              onChange={onChangeTel}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label>Memo:</label>
+            <input
+              type="text"
+              onChange={onChangeMemo}
+              className="form-control"
+            />
+          </div>
+        </div>
+        <button onClick={doAction} className="btn btn-primary">
+          Add
+        </button>
+        <button onClick={goBack} className="btn">
+          Go Back
+        </button>
+      </Layout>
+    </>
+  );
+}
